477a374774496d7b51c5b94ac11f443a49e180cd
Dianne <mdianne.gabriel@gmail.com>
2021-08-31 19:14:11 -0400

    Update InterviewerList return

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,6 @@
 import React from "react";
-import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss"
+import InterviewerListItem from "components/InterviewerListItem";
 
 export default function InterviewerList(props) {
   const interviewers = props.interviewers.map(interviewer => {
@@ -8,11 +8,16 @@ export default function InterviewerList(props) {
       <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name}
-        spots={interviewer.spots}
-        selected={interviewer.name === props.interviewer}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
         setInterviewer={() => props.setinterviewer(interviewer.id)}
       />
     )
   })
-  return interviewers;
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  )
 }
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -6,18 +6,17 @@ import "components/InterviewerListItem.scss"
 export default function InterviewerListItem(props) {
   const interviewerClass = classNames(
     "interviewers__item", {
-      "interviewers_item--selected": props.selected
+      "interviewers__item--selected": props.selected
     }
   )
-
   return (
-  <li className={interviewerClass}>
-  <img
-    className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
-  />
-    Sylvia Palmer
-  </li>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+        {props.selected && props.name}
+    </li>
   )
 }
