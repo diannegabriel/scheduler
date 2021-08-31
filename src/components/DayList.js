8edc939b5a7615f4e9ea5fc0d894da648eba6e2f
Dianne <mdianne.gabriel@gmail.com>
2021-08-31 16:18:00 -0400

    Update

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,3 +1,9 @@
-export default function DayList() {
-  return <ul></ul>
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  return (
+    <ul>
+    </ul>
+  )
 }
