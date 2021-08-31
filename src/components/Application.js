592d4e6f745d141866da6ac2f4af6591bdb4ba11
Dianne <mdianne.gabriel@gmail.com>
2021-08-31 16:53:43 -0400

    Use useState instead on DayList props

--- src/components/Application.js
+++ src/components/Application.js
@@ -23,6 +23,7 @@ export default function Application(props) {
     },
   ];
 
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
@@ -35,8 +36,8 @@ export default function Application(props) {
       <nav className="sidebar__menu">
         <DayList
           days={days}
-          day={"Monday"}
-          setDay={day => console.log(day)}
+          day={day}
+          setDay={setDay}
         />
       </nav>
       <img
