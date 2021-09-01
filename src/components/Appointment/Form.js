0b2794927e490e01ce4e2b4689e8d63821d40dab
Dianne <mdianne.gabriel@gmail.com>
2021-09-01 13:57:44 -0400

    Create reset function in Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,7 +5,11 @@ import Button from "../Button";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "")
   const [interviewer, setInterviewer] = useState(props.interviewer || null)
-  console.log(props)
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  }
   return (
   <main className="appointment__card appointment__card--create">
     <section className="appointment__card-left">
@@ -19,7 +23,12 @@ export default function Form(props) {
           onChange={(event) => setName(event.target.value)}
         />
       </form>
-      <InterviewerList interviewers={props.interviewers} value={interviewer} setInterviewer={setInterviewer} />
+      <InterviewerList 
+        interviewers={props.interviewers} 
+        value={interviewer} 
+        interviewer={interviewer}
+        setInterviewer={setInterviewer} 
+      />
     </section>
     <section className="appointment__card-right">
       <section className="appointment__actions">
