06733e8de8b8153348d8526f2d290f9cff76c277
Dianne <mdianne.gabriel@gmail.com>
2021-09-02 13:45:02 -0400

    Implement getAppointmentsForDay. Add test for getAppointmentsForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,13 @@
-export default function getAppointmentsForDay (state, day) {
-
+export function getAppointmentsForDay(state, day) {
+	const result = [];
+	let appointmentArray = [];
+	for (const selectedDay of state.days) {
+		if (selectedDay.name === day) {
+			appointmentArray = selectedDay.appointments;
+		}
+	}
+	for (const appointment of appointmentArray) {
+		result.push(state.appointments[appointment]);
+	}
+	return result;
 }
