15b03cc40726a049c6520ddcdd218982a84551f4
Dianne <mdianne.gabriel@gmail.com>
2021-09-02 14:14:55 -0400

    Clear out repeatitive useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -54,16 +54,14 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  // const interviewers = [
-  //   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  //   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  //   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  //   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  //   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-  // ];
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  const setDay = day => setState({...state, day})
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get("/api/days")
@@ -83,8 +81,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days}
-          day={day}
+          days={state.days}
+          day={state.day}
           setDay={setDay}
         />
       </nav>
