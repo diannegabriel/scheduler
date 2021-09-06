f6f56cbd681cbdc2e5be6bdfa9449b8ee4b9d3be
Dianne <mdianne.gabriel@gmail.com>
2021-09-06 14:25:25 -0400

    Implement save and bookInterview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -10,31 +10,34 @@ export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
-
-
-   const { mode, transition, back } = useVisualMode(
+  const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
-
+  const save = (name, interviewer) => {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    props.bookInterview(props.id, interview);
+    transition(SHOW);
+  }
   return (
     <article className="appointment">
       <Header time={props.time} />
-        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-        {mode === SHOW && (
-          <Show
-            student={props.interview.student}
-            interviewer={props.interview.interviewer}
-          />
-        )}
-        {mode === CREATE && 
-          <Form 
-            onCancel={() => back()}interviewers={props.interviewers} />}
-      {/* {props.interview ? (
-        <Show student={props.interview.student} interviewer={props.interview.interviewer}/>
-      ) : (
-        <Empty />
-      )} */}
-      
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+       {mode === CREATE && (
+        <Form
+          onCancel={() => back()}
+          interviewers={props.interviewers}
+          onSave={save}
+        />
+      )}
     </article>
   )
 }
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,18 +4,17 @@ import InterviewerListItem from "components/InterviewerListItem";
 
 export default function InterviewerList(props) {
 
-  const interviewers = []
-  // const interviewers = props.interviewers.map(interviewer => {
-  //   return (
-  //     <InterviewerListItem
-  //       key={interviewer.id}
-  //       name={interviewer.name}
-  //       avatar={interviewer.avatar}
-  //       selected={interviewer.id === props.interviewer}
-  //       setInterviewer={event => props.setInterviewer(interviewer.id)}
-  //     />
-  //   );
-  // });
+  const interviewers = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={event => props.setInterviewer(interviewer.id)}
+      />
+    );
+  });
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
