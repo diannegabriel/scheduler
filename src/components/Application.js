2cefd4d3694a26cf8ed8f0baa7670298dbff8e7d
Dianne <mdianne.gabriel@gmail.com>
2021-09-06 18:38:19 -0400

    Clean up code

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
@@ -8,62 +7,6 @@ import "components/Appointment"
 import "components/Application.scss";
 
 export default function Application(props) {
-  // const [state, setState] = useState({
-  //   day: "Monday",
-  //   days: [],
-  //   appointments: {},
-  //   interviewers: []
-  // });
-
-  // const setDay = day => setState({...state, day}) 
-
-  // useEffect(() => {
-  //   Promise.all([
-	// 		axios.get("/api/days"),
-	// 		axios.get("/api/appointments"),
-	// 		axios.get("/api/interviewers")
-	// 	]).then(all => {
-	// 		setState(prev => ({days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
-	// 	});
-  // }, [])
-
-  // const bookInterview = (id, interview) => {
-  //   console.log(id, interview);
-  //   const appointment = {
-  //     ...state.appointments[id],
-  //     interview: { ...interview }
-  //   }
-  //   const appointments = {
-  //     ...state.appointments,
-  //     [id]: appointment
-  //   }
-  //   return axios.put(`/api/appointments/${id}`, appointment)
-  //     .then(() => {
-  //       setState({
-  //         ...state,
-  //         appointments
-  //       })
-  //     })
-  // }
-
-  // const cancelInterview = (id) => {
-  //   const appointment = {
-  //     ...state.appointments[id],
-  //     interview: null
-  //   };
-  //   const appointments = {
-  //     ...state.appointments,
-  //     [id]: appointment
-  //   };
-  //   return axios.delete(`/api/appointments/${id}`).then(response => {
-  //     setState({ ...state, appointments });
-  //   });
-  // }
-
-  // const deleteInterview = () => {
-  //   return null;
-  // }
-
   const {
     state,
     setDay,
